afdbd532861d41d4b5981b47b635fe8cd80c9f3e
alexfung888 <1.alexfung@gmail.com>
2016-08-16 12:14:43 +0800

    Create new activity again with all files
    video asked Blank Activity with Fragment template
    none.
    created activity using Basic Activity
    because Empty Activity does not have hierarchical parent option
    
    Undo and try a second time. Still Basic Activity.
    This time checked "with fragment"
    However, video has more template code about option menu not present here. See diff:
    https://github.com/udacity/Sunshine-Version-2/compare/3.01_add_toast...3.02_create_detail_activity
    
    Forgotten to check all files in the previous two commits

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -9,5 +9,7 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
