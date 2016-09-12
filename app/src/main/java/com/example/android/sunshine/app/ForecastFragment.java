686da241ad045fdb09e004fcb8593d6c74204d46
alexfung888 <1.alexfung@gmail.com>
2016-09-12 14:38:10 +0800

    minor touch up to IP() static function call

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -138,30 +138,29 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
+    private static InetAddress ip() throws SocketException {
+        Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
+        NetworkInterface ni;
+        while (nis.hasMoreElements()) {
+            ni = nis.nextElement();
+            if (!ni.isLoopback()/*not loopback*/ && ni.isUp()/*it works now*/) {
+                for (InterfaceAddress ia : ni.getInterfaceAddresses()) {
+                    //filter for ipv4/ipv6
+                    if (ia.getAddress().getAddress().length == 4) {
+                        //4 for ipv4, 16 for ipv6
+                        return ia.getAddress();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
     public class fetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = fetchWeatherTask.class.getSimpleName();
 
         private final int numDays = 7;
-
-        private InetAddress ip() throws SocketException {
-            Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
-            NetworkInterface ni;
-            while (nis.hasMoreElements()) {
-                ni = nis.nextElement();
-                if (!ni.isLoopback()/*not loopback*/ && ni.isUp()/*it works now*/) {
-                    for (InterfaceAddress ia : ni.getInterfaceAddresses()) {
-                        //filter for ipv4/ipv6
-                        if (ia.getAddress().getAddress().length == 4) {
-                            //4 for ipv4, 16 for ipv6
-                            return ia.getAddress();
-                        }
-                    }
-                }
-            }
-            return null;
-        }
-
 
         protected String[] doInBackground(String... params) {
 
@@ -224,13 +223,13 @@ public class ForecastFragment extends Fragment {
                 } catch (SocketException e) {
                     Log.e(LOG_TAG,"getIP results in exception", e);
                 }
-                if (myIP.startsWith("10.15.",1)) {
+                if (myIP.startsWith("/10.15.")) {
                     // EDB WCH LAN
                     Properties systemProperties = System.getProperties();
                     systemProperties.setProperty("http.proxyHost","wch-tmg02.edb.local");
                     systemProperties.setProperty("http.proxyPort","8080");
                     Log.v(LOG_TAG, "proxy set to WCH LAN");
-                } else if (myIP.startsWith("192.168.20.",1)) {
+                } else if (myIP.startsWith("/192.168.20.")) {
                     // 3042bb
                     Properties systemProperties = System.getProperties();
                     systemProperties.setProperty("http.proxyHost","192.168.20.250");
